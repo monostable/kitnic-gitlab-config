afdabc21faa62fe9a122228faabac4daadc70619
gitlab-root <root@gitlab>
2017-04-19 23:05:59 +0000

    Add smtp settings

--- etc/gitlab/gitlab.rb
+++ etc/gitlab/gitlab.rb
@@ -386,6 +386,18 @@ gitlab_rails['omniauth_providers'] = [
 # gitlab_rails['smtp_enable_starttls_auto'] = true
 # gitlab_rails['smtp_tls'] = false
 
+gitlab_rails['smtp_enable'] = true
+gitlab_rails['smtp_address'] = "mail.gandi.net"
+gitlab_rails['smtp_port'] = 587
+gitlab_rails['smtp_authentication'] = "plain"
+gitlab_rails['smtp_enable_starttls_auto'] = true
+gitlab_rails['smtp_user_name'] = "accounts@kitnic.it"
+gitlab_rails['smtp_password'] = 
+gitlab_rails['smtp_domain'] = "kitnic.it"
+gitlab_rails['gitlab_email_from'] = 'accounts@kitnic.it'
+gitlab_rails['gitlab_email_reply_to'] = 'accounts@kitnic.it'
+gitlab_rails['gitlab_email_display_name'] = 'kitnic.it'
+
 ###! **Can be: 'none', 'peer', 'client_once', 'fail_if_no_peer_cert'**
 ###! Docs: http://api.rubyonrails.org/classes/ActionMailer/Base.html
 # gitlab_rails['smtp_openssl_verify_mode'] = 'none'
