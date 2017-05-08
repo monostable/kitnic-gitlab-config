88026a28d29a7c292353abb80908a8fdd100d171
gitlab-root <root@gitlab>
2017-05-08 15:27:19 +0000

    Change email config

--- etc/gitlab/gitlab.rb
+++ etc/gitlab/gitlab.rb
@@ -387,14 +387,14 @@ gitlab_rails['omniauth_providers'] = [
 # gitlab_rails['smtp_tls'] = false
 
 gitlab_rails['smtp_enable'] = true
-gitlab_rails['smtp_address'] = "mail.gandi.net"
+gitlab_rails['smtp_address'] = "smtp.mailgun.org"
 gitlab_rails['smtp_port'] = 587
 gitlab_rails['smtp_authentication'] = "plain"
 gitlab_rails['smtp_enable_starttls_auto'] = true
-gitlab_rails['smtp_user_name'] = "accounts@kitnic.it"
+gitlab_rails['smtp_user_name'] = "accounts@mailout.kitnic.it"
 gitlab_rails['smtp_password'] = 
-gitlab_rails['smtp_domain'] = "kitnic.it"
-gitlab_rails['gitlab_email_from'] = 'accounts@kitnic.it'
+gitlab_rails['smtp_domain'] = "mailout.kitnic.it"
+gitlab_rails['gitlab_email_from'] = 'accounts@mailout.kitnic.it'
 gitlab_rails['gitlab_email_reply_to'] = 'accounts@kitnic.it'
 gitlab_rails['gitlab_email_display_name'] = 'kitnic.it'
 
